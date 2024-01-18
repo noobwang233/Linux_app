7fad09b168af70c5993d77559ef6f152fa44b68e
noobwang233 <1213236870@qq.com>
2024-01-18 15:03:46 +0800

    finish simple IO test

--- Simple_IO/test03/test03.c
+++ Simple_IO/test03/test03.c
@@ -5,3 +5,22 @@
 使用只写方式打开文件，将文件前1Kbyte字节数据填充为0x00，将下1Kbyte字节数据填充为0xFF，操作完成之后显式关闭文件，退出程序。
 
 */
+
+
+#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/stat.h> 
+
+#define ASSERT(x) if((x) < 0){return x;}
+
+int main(int argc, char *argv[])
+{
+    int retvalue;
+    int fd;
+
+   fd = open(argv[1], O_WRONLY | O_CREAT | O_EXCL, S_IRWXU | S_IRGRP | S_IROTH);
+   ASSERT(fd)
+
+   ...
+
+}
