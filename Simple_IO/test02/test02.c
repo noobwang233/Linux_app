e139ae23ea78bf89e99f731ce16f6f53457bb87d
noobwang233 <1213236870@qq.com>
2024-01-18 14:44:15 +0800

    save test02.c

--- Simple_IO/test02/test02.c
+++ Simple_IO/test02/test02.c
@@ -2,3 +2,27 @@
 2. 通过open函数判断文件是否存在（例如test_file），并将判断结果显示出来
 
 */
+
+#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/stat.h> 
+
+#define ASSERT(x) if((x) < 0){return x;}
+
+int main(int argc, char *argv[])
+{
+    int retvalue;
+    int fd;
+
+    fd = open(argv[1], O_RDONLY);
+    if(fd < 0)
+    {
+        printf("file doesn't exsit !\n");
+    }
+    else
+    {
+        printf("file exsit !\n");
+    }
+
+
+}
