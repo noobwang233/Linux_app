fbe06dcda8130134554b62becd6ab0aaf6688da2
noobwang233 <1213236870@qq.com>
2024-01-19 13:47:40 +0800

    finish test03

--- Simple_IO/test03/test03.c
+++ Simple_IO/test03/test03.c
@@ -5,22 +5,34 @@
 使用只写方式打开文件，将文件前1Kbyte字节数据填充为0x00，将下1Kbyte字节数据填充为0xFF，操作完成之后显式关闭文件，退出程序。
 
 */
-
-
 #include <stdio.h>
 #include <sys/types.h> 
 #include <sys/stat.h> 
+#include <fcntl.h> 
+#include <unistd.h> 
 
-#define ASSERT(x) if((x) < 0){return x;}
+#define ASSERT(x) do{if((x) < 0){return x;}}while (0)
+#define SETBUF(buf, x) do{ for(unsigned int i = 0; i < sizeof(buf); i++){ (buf)[i] = (x); }}while(0)
 
 int main(int argc, char *argv[])
 {
-    int retvalue;
-    int fd;
+   int retvalue;
+   int fd;
+   unsigned char buf[1024];
 
    fd = open(argv[1], O_WRONLY | O_CREAT | O_EXCL, S_IRWXU | S_IRGRP | S_IROTH);
-   ASSERT(fd)
+   ASSERT(fd);
 
-   ...
+   SETBUF(buf, 0x00);
+   retvalue = write(fd, buf, sizeof(buf));
+   ASSERT(retvalue);
+   printf("write num: %d\n", retvalue);
 
+   SETBUF(buf, 0xFF);
+   retvalue = write(fd, buf, sizeof(buf));
+   ASSERT(retvalue);
+   printf("write num: %d\n", retvalue);
+
+   retvalue = close(fd);
+   ASSERT(retvalue);
 }
