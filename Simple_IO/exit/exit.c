3e57d56c398920fe998a5d1436a04fb8450f70eb
noobwang233 <1213236870@qq.com>
2024-01-19 14:34:18 +0800

    finish exit test

---
+++ Simple_IO/exit/exit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <fcntl.h> 
+#include <unistd.h> 
+#include <errno.h>
+
+#define ASSERT(x) do{if((x) < 0){ perror(""); exit(errno);}}while (0)
+
+
+static void fun(const char* file)
+{
+    int fd;
+
+    printf("function:\n");
+    fd = open(file, O_RDONLY);
+    ASSERT(fd);
+} 
+
+//return 只能在main函数中退出程序，而exit可以在任何函数里退出程序
+int main(int argc, char *argv[])
+{
+    int retvalue;
+    int fd;
+
+    fun(argv[1]);
+
+    printf("return:\n");
+    return 0;
+}
