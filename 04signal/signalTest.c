e9cd16de3dce4bf5a0e1edb2a523448d99596e2c
noobwang233 <1213236870@qq.com>
2024-01-24 17:26:10 +0800

    temp save

--- 04signal/signalTest.c
+++ 04signal/signalTest.c
@@ -1,6 +1,9 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <signal.h>
+#include <errno.h>
+
+#define ASSERT(x) do{if((x) < 0){ perror(""); exit(errno);}}while (0)
 
 static void sig_handler(int sig) 
 { 
@@ -9,15 +12,24 @@ static void sig_handler(int sig)
 
 int main(int argc, char *argv[]) 
 { 
-    sig_t ret = NULL; 
+    // sig_t ret = NULL; 
     
-    ret = signal(SIGINT, (sig_t)sig_handler); 
-    if (SIG_ERR == ret) 
-    {
-        perror("signal error"); exit(-1); 
-    } 
-    
+    // ret = signal(SIGINT, (sig_t)sig_handler); 
+    // if (SIG_ERR == ret) 
+    // {
+    //     perror("signal error"); 
+    //     exit(-1); 
+    // }
+
+    int ret;
+    struct sigaction sigact;
+    sigact.sa_handler = sig_handler;
+    sigact.sa_flags = 0;
+    ret = sigaction(SIGINT, &sigact, NULL);
+    ASSERT(ret);
+
     /* 死循环 */ 
-    for ( ; ; ) { } 
+    for ( ; ; ) { }
+
     exit(0); 
 }
