df529a93af539f132623568a0ddb197aaa65a92d
noobwang233 <1213236870@qq.com>
2024-02-27 18:03:55 +0800

    save

--- Imx6ull/gpio.c
+++ Imx6ull/gpio.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+#define CONFIG_CMD  0
+#define WRITE_CMD   1
+#define READ_CMD    2
+#define TRIGGER_CMD 3
 #define USAGE_ERROR(cmd, value) do{ \
                                         printf("usage error, usage: %s %s gpiox %s\n", argv[0],\
                                                 (((cmd) == NULL) ? "<config/write/read/trigger>" : cmd), \
                                                 (((value) == NULL) ? "<value>" : value));\
                                     }while(0)
 
-static int CheckArg(int argc, char *argv[], int cmd, int value)
+
+
+
+
+
+static int CheckArg(int argc, char *argv[], int *cmd, int *value)
 {
     char *cmds = NULL;
     char *values = NULL;
@@ -39,7 +47,7 @@ int main(int argc, char *argv[])
 {
     int ret, cmd, value;
 
-    ret = CheckArg(argc, argv, cmd, value);
+    ret = CheckArg(argc, argv, &cmd, &value);
     if(ret != 0)
     {
         printf("check args error, exit\n");
